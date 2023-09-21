03c062dc197b6a162acfa5dcd7eecbdc9d1c14bd
Filipe Alves <lipeqa@gmail.com>
2023-09-21 11:37:43 -0300

    feat: aprendendo terminal e Argumentos

---
+++ sintaxe-java/src/edu/dio/terminalArgumentos/TerminalArgumento.java
@@ -0,0 +1,8 @@
+package edu.dio.terminalArgumentos;
+
+public class TerminalArgumento {
+    public static void main(String[] args) {
+        System.out.println("Oi fui execultado pelo terminal!!!^^");
+        
+    }
+}
